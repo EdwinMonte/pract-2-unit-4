14fb84866031470b9c690caf8ca40087adb66701
drp_3 <drp_3@DESKTOP-T87EQCP>
2024-03-04 21:47:32 -0600

    Agregar archivos de proyecto.

---
+++ pract 2 unit 4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Perro miPerro = new Perro();
+        miPerro.Nombre = "Noah";
+        miPerro.Comer();  // Método heredado de la clase Animal
+        miPerro.Ladrar(); // Método específico de la clase Perro
+
+        Console.ReadKey();
+    }
+}
+
---
+++ pract 2 unit 4/perro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Clase base
+public class Animal
+{
+    public string Nombre { get; set; }
+
+    public void Comer()
+    {
+        Console.WriteLine($"{Nombre} está comiendo.");
+    }
+}
+
+// Clase que hereda de Animal
+public class Perro : Animal
+{
+    public void Ladrar()
+    {
+        Console.WriteLine($"{Nombre} está ladrando.");
+    }
+}
+
